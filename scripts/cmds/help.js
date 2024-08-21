c00f99c57102623aa3bd0ef4a989877a14e8377e
Solaris-udo <solariscrimson10@gmail.com>
2024-08-21 07:33:28 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ ☆ | ♥ 𝐋𝐀𝐔𝐑𝐈𝐀𝐍𝐄 ✰𝐁𝐎𝐓 ♥ | ☆]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[ ☆ | 🍸𝐒𝐎𝐋𝐀𝐑𝐈𝐒✰𝐁𝐎𝐓 🍭 | ☆]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 module.exports = {
 	config: {
 		name: "help",
@@ -59,17 +59,17 @@ module.exports = {
 			});
 
 			const totalCommands = commands.size;
-			msg += `\n𝐋𝐚𝐮𝐫𝐢𝐚𝐧𝐞☆𝐁𝐨𝐭 𝐡𝐚𝐬  ${totalCommands} 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬 ✔\n`;
+			msg += `\n𝐒𝐎𝐋𝐀𝐑𝐈𝐒☆𝐁𝐨𝐭 𝐡𝐚𝐬  ${totalCommands} 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬 ✔\n`;
 			msg += `${prefix}𝐡𝐞𝐥𝐩 𝐭𝐨 𝐥𝐨𝐨𝐤 𝐜𝐦𝐝𝐬\n`;
           msg += `𝐀𝐧𝐲 𝐩𝐫𝐨𝐛𝐥𝐞𝐦 𝐫𝐞𝐥𝐚𝐭𝐞𝐝 𝐭𝐨 𝐛𝐨𝐭 𝐭𝐡𝐞𝐧 𝐮𝐬𝐞 ${prefix}𝐜𝐚𝐥𝐥𝐚𝐝\n`;
-			msg += `Admin :👑☆𝐌𝐥𝐝☆𝐄𝐦𝐦𝐚𝐧𝐮𝐞𝐥☆\n\n`; // its not decoy so change it if you want
+			msg += `Admin :👑☆𝐒𝐀𝐏𝐈𝐄𝐍𝐒☆𝐉𝐂 𝐁𝐈𝐎𝐒𝐇𝐈☆\n\n`; // its not decoy so change it if you want
           msg += `❦ 𝐌𝐄𝐑𝐂𝐈☆𝐁𝐄𝐀𝐔𝐂𝐎𝐔𝐏❦
 
 ᕯᕯ𝒎𝒂𝒅𝒆 𝒃𝒚ᕯᕯ 
 
-[👑| 𝐌𝐥𝐝☆𝐄𝐦𝐦𝐚𝐧𝐮𝐞𝐥 ]`;
+[🏀| 𝐒𝐀𝐏𝐈𝐄𝐍𝐒 𝐉𝐂 ]`;
 
-msg += `𝐅𝐛: ✰ m.me/100085261760009 ☆ `;
+msg += `𝐅𝐛: https//m.me/61557502971632 ☆ `;
 			await message.reply({
 				body: msg,
 			});
@@ -90,7 +90,7 @@ msg += `𝐅𝐛: ✰ m.me/100085261760009 ☆ `;
 				const guideBody = configCommand.guide?.en || "No guide available.";
 				const usage = guideBody.replace(/{p}/g, prefix).replace(/{n}/g, configCommand.name);
 
-const response = `✣━☆𝐋𝐚𝐮𝐫𝐢𝐚𝐧𝐞♡𝐁𝐨𝐭☆━✤      
+const response = `✣━☆𝐒𝐎𝐋𝐀𝐑𝐈𝐒♡𝐁𝐨𝐭☆━✤      
                  🅒🅜🅓☆🅘🅝🅕🅞
 
 ❐𝙉𝙖𝙢𝙚 ➢${configCommand.name}
